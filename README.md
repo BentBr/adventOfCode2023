404048b0b62667a09c2010229b101bbac578ae20
Bent Brüggemann <bent.brueggemann@blackbit.de>
2023-12-02 18:47:10 +0100

    Added loop for all days + colouring

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Advent of Code 2023
 
+## This year's advent
+It's about the questions from [advent of code](https://adventofcode.com/2023)
+## Commands
 Run `cargo run` to get the solutions of all the days. \
 `cargo run <day>` to get the solution of given day
+
--- src/solution_selector.rs
+++ src/solution_selector.rs
@@ -1,22 +1,33 @@
 use super::days;
 
 pub fn get_solution(day: u8) {
-    println!("Chosen solution for day '{}'...", day);
+    // Defining green colour
+    println!("\u{001b}[32mChosen solution for day '{}'...", day);
 
     load_solution(day);
 }
 
 pub fn get_all_solutions() {
-    println!("Getting all solutions...")
+    // Defining green colour
+    println!("\u{001b}[32mGetting all solutions...");
+
+    for day in 1..=2 {
+        load_solution(day)
+    }
 }
 
 fn load_solution(day: u8) {
     // Dynamically load the appropriate solutions based on the provided day
+    // Defining red colour
+    print!("\u{001b}[31m");
     match day {
         1 => days::day_1::solutions(),
         2 => days::day_2::solutions(),
         _ => {
-            println!("No solutions available for day {} (yet)", day);
+            // Resetting the colour
+            print!("\u{001b}[0m");
+            // Red again (just because I can
+            println!("\u{001b}[31mNo solutions available for day {} (yet)", day);
         }
     }
 }
